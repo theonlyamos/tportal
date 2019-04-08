28312f3045bb90f36464fd290a61bcaa26ad30f8
jamesstefens18 <fireisaverywickedmaster@gmail.com>
2019-04-08 19:28:46 +0000

    fixed double PHPMailer class import

--- register.php
+++ register.php
@@ -1,13 +1,5 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-require 'PHPMailer/src/Exception.php';
-
 require_once 'functions.php';
 
 
